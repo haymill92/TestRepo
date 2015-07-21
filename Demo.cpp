00c1527a0d9664ea9b31c823e04098f4a6a0eaad
Hayden Miller <haydenkylemiller@gmail.com>
2015-07-20 23:39:17 -0500

    imgproc

--- Demo.cpp
+++ Demo.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video.hpp>
 #include <opencv2/opencv.hpp>
 #include <time.h>
