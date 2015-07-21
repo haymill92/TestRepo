1011f2168631aefb7e2078ff1603101ae55c1905
Hayden Miller <haydenkylemiller@gmail.com>
2015-07-20 23:40:17 -0500

    video

--- Demo.cpp
+++ Demo.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/video.hpp>
+#include <opencv2/video/video.hpp>
 #include <opencv2/opencv.hpp>
 #include <time.h>
 
