3faf373868bcb5d3d8f2539b8a1dcde7bc09c70e
Hayden Miller <haydenkylemiller@gmail.com>
2015-07-20 23:24:10 -0500

    Update Headers

--- Demo.cpp
+++ Demo.cpp
@@ -1,9 +1,9 @@
-#include "opencv2/objdetect.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include <opencv2/video.hpp>
-
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/opencv.hpp>
 #include <time.h>
 
 #include <iostream>
